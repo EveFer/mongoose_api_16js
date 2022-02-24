e40aa21757616bf6e9afc2b69610cb8452fe6cc8
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-02-23 22:11:16 -0600

    server init

---
+++ index.js
@@ -0,0 +1,21 @@
+// Generar un endpoint que permita listar
+const express = require('express')
+
+const server =  express()
+
+server.use(express.json()) //
+
+
+server.get('/koders', (request, response) => {
+    // 
+    response.json({
+        success: true,
+        data: {
+            koders: []
+        }
+    })
+})
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080')
+})
